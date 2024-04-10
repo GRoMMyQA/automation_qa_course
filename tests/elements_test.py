d2dddb7cba238af0fc6b57bfdaa916764a3fec2f
Roman <romaholovai@gmail.com>
2024-04-10 18:57:46 +0200

    second day making tests for internet page with four text fields and one button, then I check input and output data of equality. Makes generator of person info

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,9 +1,21 @@
 import time
 
-from pages.base_page import BasePage
+from pages.elements_page import TextBoxPage
 
+class TestElements:
+    class TestTextBox:
 
-def test(driver):
-    page = BasePage(driver, 'https://www.google.com')
-    page.open()
-    time.sleep(3)
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_name, output_email, output_current_address, output_permanent_address = text_box_page.check_field_form()
+            assert full_name == output_name, "the full name does not match"
+            assert email == output_email, "the email does not match"
+            assert current_address == output_current_address, "the current address does not match"
+            assert permanent_address == output_permanent_address, "the permanent address does not match"
+
+            # or
+            # input_data = text_box_page.fill_all_fields()
+            # output_data = text_box_page.check_field_form()
+            # assert input_data == output_data
